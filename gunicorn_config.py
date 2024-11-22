5ef168b3f0c53e2f10f77370d5a92f73bd377b37
Christian DaLon Esplin <cespn20@gmail.com>
2024-11-22 11:30:47 -0800

    Add Procfile and update gunicorn config for Render deployment

--- gunicorn_config.py
+++ gunicorn_config.py
@@ -1,4 +1,6 @@
-bind = "0.0.0.0:10000"
-workers = 2
+bind = "0.0.0.0:$PORT"
+workers = 4
 threads = 4
 timeout = 120
+accesslog = '-'
+access_log_format = '%(h)s %(l)s %(u)s %(t)s "%(r)s" %(s)s %(b)s "%(f)s" "%(a)s"'
